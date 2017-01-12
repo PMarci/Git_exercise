9a4e788c388ec7c698d604330323d095b49e7515
Rita Pasztor <pasztorrita@gmail.com>
2017-01-12 16:00:02 +0100

    b3

--- src/RitaAge.java
+++ src/RitaAge.java
@@ -4,7 +4,7 @@
 public class RitaAge {
     public void count() {
         int age = 28;
-        System.out.println("My age is: "+ age + " years");
+        System.out.println("My age is: " + age + " years");
     }
 
 }
